c714fb03e263efba06357f219ee528f62958f87b
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-16 20:18:24 +0530

    Group Sold Products By The Date

---
+++ 3/3.sql
@@ -0,0 +1,6 @@
+-- 1667
+-- https://leetcode.com/problems/fix-names-in-a-table/
+
+SELECT USER_ID, CONCAT(UPPER(SUBSTRING(NAME,1,1)), 
+LOWER(SUBSTRING(NAME,2,LENGTH(NAME)-1))) AS NAME
+FROM USERS ORDER BY USER_ID
