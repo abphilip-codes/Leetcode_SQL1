dc0afe618cea97e0ab542a5ce38a488086e90852
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-18 16:07:44 +0530

    Combine Two Tables

---
+++ 5/3.sql
@@ -0,0 +1,8 @@
+-- 1148
+-- https://leetcode.com/problems/article-views-i/
+
+SELECT ID, CASE
+    WHEN P_ID IS NULL THEN 'Root'
+    WHEN ID IN (SELECT P_ID FROM TREE) 
+    THEN 'Inner' ELSE 'Leaf' END AS TYPE
+FROM TREE ORDER BY ID;
