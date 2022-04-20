e50153ff330e927d27000956ac4d24945e6f2e5d
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-20 11:09:21 +0530

    Find Total Time Spent by Each Employee

--- 8/4.sql
+++ 8/4.sql
@@ -1,5 +1,5 @@
--- 1729
--- https://leetcode.com/problems/find-followers-count/
+-- 1741
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
 
-SELECT USER_ID, COUNT(FOLLOWER_ID) AS FOLLOWERS_COUNT
-FROM FOLLOWERS GROUP BY USER_ID ORDER BY USER_ID;
+SELECT EVENT_DAY AS DAY, EMP_ID, SUM(OUT_TIME-IN_TIME) 
+AS TOTAL_TIME FROM EMPLOYEES GROUP BY EVENT_DAY, EMP_ID;
