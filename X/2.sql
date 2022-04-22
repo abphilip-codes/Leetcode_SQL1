8d1ef4b57476cfff80c9e6f883e9b821c249bb96
Allen Ben Philipose <allenalvin333@gmail.com>
2022-04-22 10:29:15 +0530

    Actors and Directors Who Cooperated At Least Three Times

--- X/2.sql
+++ X/2.sql
@@ -2,5 +2,4 @@
 -- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
 
 SELECT ACTOR_ID, DIRECTOR_ID FROM ACTORDIRECTOR
-GROUP BY ACTOR_ID, DIRECTOR_ID
-HAVING COUNT(*) >= 3;
+GROUP BY ACTOR_ID, DIRECTOR_ID HAVING COUNT(*) >= 3;
